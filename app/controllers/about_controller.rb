60d1dc39ae0bca5e415076346ba4bddb8c88b393
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2024-06-12 10:58:07 -0400

    Set up basic staging area. Added html to standard about page

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,6 @@
+class AboutController < ApplicationController
+    def index
+        # doesn't need to do anything because inheriting from ApplicationController
+        # when this is processed it will say if this does any rendering then use that, if not use rails default render 
+    end
+end
