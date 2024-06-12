5f1e3c3b4b201fe3d76de461330c4059c71db2da
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2024-06-12 12:03:05 -0400

    Using link to helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get "about", to: "about#index"
+  # if you wanted to change about to about-us do this:
+  # get "about-us" to: "about#index" as: :about
+
+
+  # the default rails page only works in development
+  # will give you a 404 error if you try to use without a route
+  # get "/", to: "main#index"
+  # create a main controller with an index action 
+  # the / is optional and you can do get "", to...
+  # can also do:
+  root to: "main#index"
+end
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,15 +23,3 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
-  get "about", to: "about#index"
-
-  # the default rails page only works in development
-  # will give you a 404 error if you try to use the app without a # route
-
-  # get "/", to: "main#index"
-  # create a main controller with an index action 
-  # the / is optional and you can do get "", to...
-  # can also do:
-  root to: "main#index"
-end
